cf886d03990b916b198d28fde9e2dcee95c86520
Emiliano Firmino <emiliano.firmino@gmail.com>
2020-04-13 16:22:45 +0200

    Init goteborg
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

---
+++ include/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ * Copyright (C) 2020 Emiliano Firmino <emiliano.firmino@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc, char** argv);
+
+#endif /* !MAIN_H */
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ * Copyright (C) 2020 Emiliano Firmino <emiliano.firmino@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "main.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}

de9b93798bc9f572afbb3707170f07b08d54a7f0
Emiliano Firmino <emiliano.firmino@gmail.com>
2020-04-13 16:42:36 +0200

    Setup conan package manager
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+/*
+ * main.cpp
+ * Copyright (C) 2020 Emiliano Firmino <emiliano.firmino@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
